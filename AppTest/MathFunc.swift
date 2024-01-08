2d40529a11dedb3379c1bfcf6fa74cd79b623b88
itsmylifestyle <hahaii00@mail.ru>
2024-01-08 21:50:01 +0600

    test deletion

---
+++ AppTest/MathFunc.swift
@@ -0,0 +1,30 @@
+//
+//  MathFunc.swift
+//  AppTest
+//
+//  Created by Айбек on 08.01.2024.
+//
+
+import Foundation
+
+
+class mathFunc {
+    func addInteger(x: Int, y: Int) -> Int {
+        return x + y
+    }
+    
+    func multInteger(x: Int, y: Int) -> Int {
+        return x * y
+    }
+    
+    func divInteger(x: Int, y: Int) -> Int {
+        return x / y
+    }
+    
+    func minInteger(x: Int, y: Int) -> Int {
+        return x - y
+    }
+}
+
+
+
--- AppTestTests/AppTestTests.swift
+++ AppTestTests/AppTestTests.swift
@@ -10,27 +10,30 @@ import XCTest
 
 final class AppTestTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    let math = mathFunc()
+    
+    func testAddFunc() {
+        let res = math.addInteger(x: 1, y: 2)
+        
+        XCTAssertEqual(res, 3)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testMultFunc() {
+        let res = math.multInteger(x: 2, y: 2)
+        
+        XCTAssertEqual(res, 4)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testDivFunc() {
+        let res = math.divInteger(x: 6, y: 2)
+        
+        XCTAssertEqual(res, 3)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testMinFunc() {
+        let res = math.minInteger(x: 5, y: 1)
+        
+        XCTAssertEqual(res, 4)
     }
 
 }
